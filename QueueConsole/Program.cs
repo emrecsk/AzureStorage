6ba817b4936dfff8117ba9df26adf53264cd03f1
Emre Coskun <Emre.Coskun@bs.nttdata.com>
2023-10-20 13:15:50 +0200

    New_Branch_Test

--- QueueConsole/Program.cs
+++ QueueConsole/Program.cs
@@ -2,11 +2,7 @@
 using AzureStrorageLibrary.Services;
 using System.Text;
 // See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-
 ConnectionString.Con = "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;";
-
-
 
 AzQueue azQueue = new AzQueue("testqueue");
 
